4754f19fb71d0cfbe015d472a902b92272ca26e0
brendadavid <brendadavid.r@gmail.com>
2024-09-27 23:10:02 -0300

    feat: adiciona configurações iniciais do projeto

---
+++ src/main/java/com/desafio/luizalabs/Application.java
@@ -0,0 +1,14 @@
+package com.desafio.luizalabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.desafio.luizalabs"})
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
